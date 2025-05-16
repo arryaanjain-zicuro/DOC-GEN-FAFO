d047039708176b5ea82abd0f6417d7ccb847c8a7
arryaanjain <jainarryaan@gmail.com>
2025-05-16 11:49:21 +0530

    (backend) directory structure a bit awful for models :/ but I just had to add an action and explanation field in models so that analysis data is robust for the next step, i.e. doc generation

--- backend/workflows/models/betaExcel/beta_excel_mapping.py
+++ backend/workflows/models/betaExcel/beta_excel_mapping.py
@@ -1,7 +1,5 @@
-# models/beta_excel_mapping.py
 from pydantic import BaseModel
-from typing import Optional
-
+from typing import Optional, Literal
 
 class BetaExcelFieldMapping(BaseModel):
     alpha_field: str
@@ -9,3 +7,5 @@ class BetaExcelFieldMapping(BaseModel):
     cell: str
     value: Optional[str]
     transformation: Optional[str]
+    action: Literal["write", "append", "calculate", "ignore"] = "write"
+    explanation: Optional[str]  # e.g. "Field is copied directly", "Sum of Alpha fields A and B"
--- backend/workflows/models/betaWord/shared/field_mapping.py
+++ backend/workflows/models/betaWord/shared/field_mapping.py
@@ -1,8 +1,9 @@
-# workflows/models/shared/field_mapping.py
-
 from pydantic import BaseModel
+from typing import Literal, Optional
 
 class BetaWordFieldMapping(BaseModel):
     alpha_field: str
     beta_occurrence: str
     transformation: str
+    action: Literal["write", "append", "ignore"] = "write"
+    explanation: Optional[str]  # e.g. "Field is copied directly", "Sum of Alpha fields A and B"
