cad4930e3a29575fb2a0b52dfd05206d338c367d
arryaanjain <jainarryaan@gmail.com>
2025-05-16 18:14:50 +0530

    (backend) removing limiting from agent, that was fatal :D, limiting should be ONLY ON PARTS CALLING GEMINI, agents and workflow of langgraph are for orchestration

--- backend/workflows/agents/alpha_agent.py
+++ backend/workflows/agents/alpha_agent.py
@@ -3,16 +3,11 @@
 from app.parsing.alpha_doc_parser import parse_alpha_document
 from workflows.models.alpha.alpha_models import ParsedAlphaDocument
 from workflows.models.shared import TransformationState
-from app.core.gemini_rate_limiter import GeminiRateLimiter
-
-limiter = GeminiRateLimiter(rate_limit_per_minute=60)
 
 def alpha_agent_node(state: TransformationState) -> TransformationState:
     alpha_path = state.alpha_path
     if not alpha_path:
         raise ValueError("Missing required 'alpha_path' in state")
-
-    limiter.wait()
 
     alpha_result: ParsedAlphaDocument = parse_alpha_document(alpha_path)
     return state.model_copy(update={"alpha_data": alpha_result})
